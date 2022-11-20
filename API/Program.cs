95bf5cd01ebf7470a594997f58a562f591d6cf3b
Lunext <eurengabriel@gmail.com>
2022-11-20 10:43:50 -0500

    CrudOperations of activities done

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
 using Persistence;
 using Microsoft.EntityFrameworkCore;
-
+using MediatR;
+using Application.Activities;
+using Application.Core;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,22 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(opt=>{
-   opt.AddPolicy("CorsPolicy",policy=>{
-    policy.AllowAnyMethod()
-    .AllowAnyHeader().WithOrigins("http://localhost:3000");
-   });
-});
-
-
-
-builder.Services.AddDbContext<DataContext>(opt=>{
-
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));  
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
